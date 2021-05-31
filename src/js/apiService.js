f622412edf2760cc2039b830b7df2ccec05f1ae6
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-05-31 21:51:26 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -9,8 +9,8 @@ export default class NewsApiService {
 
     fetchImages = () => {
      axios.defaults.baseURL = `https://pixabay.com/api/`;
-     const API_KEY = `21859800-af94843fb327cc57780ddd667`;
-     let params = `?image_type=photo&orientation=horizontal&key=${API_KEY}
+    //  const API_KEY = `21859800-af94843fb327cc57780ddd667`;
+     let params = `?image_type=photo&orientation=horizontal&key=21859800-af94843fb327cc57780ddd667
      &per_page=12&page=${this.page}&q=${this.searchQuery}`;
         return axios.get(params).then(response => response = response.data)
             .then(data => {
